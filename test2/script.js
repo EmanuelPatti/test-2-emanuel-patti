18ef00ee720d775b44f4f018258316091d26aded
EmanuelPatti <130666128+EmanuelPatti@users.noreply.github.com>
2023-04-27 17:09:39 +0200

    test-2-aggiunto

---
+++ test2/script.js
@@ -0,0 +1,16 @@
+document.getElementById('paragrafoJs').innerHTML = `La principale differenza tra js e typescript è che, TypeScript consente di definire in modo esplicito il tipo di dati associato a ogni variabile, stringa, numero , booleano. Ciò consente di ridurre gli errori causati da tipi di dati inaspettati .
+Un'altra  differenza tra TypeScript e JavaScript è che TypeScript è un linguaggio orientato agli oggetti mentre JavaScript, invece, è uno linguaggio di scripting basato sugli eventi del DOM.`
+
+// Cos'è una condizione e come si usa? Cos'è un ciclo e come li usiamo?
+let array = ['emanuel','angela','nicole','arianna','alessia','asia','francesa','chiara','sara'];
+for (let i = 0; i < array.length; i++) {
+      if (array[i].charAt(0) == 'a' && array[i].includes('n')) {
+        console.log(array[i]);
+      }
+    } 
+
+function myFunction() {
+        document.getElementById("sfondo").style.backgroundColor = 'gray';
+}
+
+
